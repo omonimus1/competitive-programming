3197d78ab9cad04d3dfcda88ea0fd85bd2575a6f
omonimus1 <davidepollicino2015@gmail.com>
2020-04-07 21:41:48 +0100

    Added new solutions

---
+++ Geekforgeeks/learning-struct.cpp
@@ -0,0 +1,10 @@
+/*structure node of linked list
+  to store data and link 
+  to the next node
+*/
+struct Node
+{
+    //Linked list has data and next members
+    int data; 
+    Node *next; 
+};
