e6ee19e982b18faa004b9067a4b453cf17fbd33c
omonimus <davidepollicino2015@gmail.com>
2020-10-09 19:34:24 +0100

    improved average of levels and level order traversal

--- Leetcode/average-of-levels-in-binary-tree.cpp
+++ Leetcode/average-of-levels-in-binary-tree.cpp
@@ -1,35 +1,37 @@
-// https://leetcode.com/problems/average-of-levels-in-binary-tree/submissions/
+//https://leetcode.com/problems/average-of-levels-in-binary-tree/submissions/
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
-        vector<double>solution;
-
-        if(root == NULL)
-            return solution;
+        vector<double>solution; 
         
-        queue<TreeNode*> q; 
-        q.push(root);
-       
-        TreeNode* node;
+        if(root == NULL)
+            return solution; 
+        
+        queue<TreeNode*>q; 
+         q.push(root); 
+        double number_nodes, sum_level; 
         while(!q.empty())
         {
-            int size = q.size();
-            vector<double>s;       
+            vector<int>level;
+            int size = q.size(); 
+            number_nodes = 0.0;
+            sum_level = 0.0; 
             while(size--)
             {
-                node = q.front();
-                q.pop();
-                s.push_back(double(node->val));
-                if(node->left != NULL) q.push(node->left);
-                if(node->right != NULL) q.push(node->right);
+                TreeNode *temp = q.front();
+                q.pop(); 
+                number_nodes+=1;
+                sum_level += temp->val; 
+                
+                if(temp->left != NULL)
+                    q.push(temp->left);
+                if(temp -> right != NULL)
+                    q.push(temp->right); 
             }
-            
-            double sum =0; 
-            for(int i =0; i < s.size(); i++) sum += s[i];
-            //solution.push_back(double(sum/s.size()));
-            solution.push_back(double(sum)/ double(s.size()));
+            solution.push_back((double(sum_level/number_nodes)));
         }
-                               
+        
         return solution; 
+        
     }
 };
--- Leetcode/binary-tree-level-order-traversal.cpp
+++ Leetcode/binary-tree-level-order-traversal.cpp
@@ -1,35 +1,46 @@
 // https://leetcode.com/problems/binary-tree-level-order-traversal/submissions/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> solution;
+        // Create vector of vector for the solution
+        vector<vector<int>>solution;
+        // If tree is empty: return empty solution; 
         if(root == NULL)
             return solution; 
-    
-        // Apply breath First search algorithm 
+        // Create queue of TreeNode* and push in the root
+        queue<TreeNode*>q; 
+        q.push(root); 
         
-        // Create a queue and push in the root
-        queue<TreeNode*>q;
-        q.push(root);
-        
-        TreeNode *node;
-        int size;
-        while(!q.empty())
+        // Until the queue is not empty
+        while(! q.empty())
         {
+            // Create a temporary vector to store the level values
+            vector<int>te;
+            int size = q.size();
             
-            size = q.size();
-            vector<int>level;
             while(size--)
             {
-                node = q.front();
-                q.pop();
-                level.push_back(node->val);
-                if(node->left != NULL) q.push(node->left);
-                if(node->right != NULL) q.push(node->right);
+                TreeNode *temp = q.front();
+                q.pop(); 
+                te.push_back(temp->val);
+                // Enqueue temporary children
+                if(temp->left != NULL ) q.push(temp->left);
+                if(temp->right != NULL) q.push(temp->right);
             }
-            
-            solution.push_back(level);
+            // Store the values of the level in the final solution; 
+            solution.push_back(te); 
         }
-        return solution;
+        return solution; 
     }
 };
