6fe1434619a24febeaa2fdc785971e15ae3fe75c
Davide Pollicino <davidepollicino2015@gmail.com>
2020-10-25 15:48:07 +0000

    Update is_it_vowel_or_consonant.cpp

---
+++ codechef/is_it_vowel_or_consonant.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/problems/VOWELTB
+// https://www.codechef.com/problems/VOWELTB
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char character;
+    cin >> character;
+    if (character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U')
+        cout << "Vowel" << endl;
+    else
+    {
+        cout << "Consonant" << endl;
+    }
+    return 0;
+}
