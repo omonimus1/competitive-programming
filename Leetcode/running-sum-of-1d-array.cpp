5b7aed5141a8c82b17fd5b3c4f6337a93cd0f030
Davide Pollicino <davidepollicino2015@gmail.com>
2022-07-11 02:08:20 +0100

    Improve solution

--- Leetcode/running-sum-of-1d-array.cpp
+++ Leetcode/running-sum-of-1d-array.cpp
@@ -1,24 +1,15 @@
-//  https://leetcode.com/problems/running-sum-of-1d-array/submissions/
+// https://leetcode.com/problems/running-sum-of-1d-array/submissions/
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int>s;
-        if(nums.size() == 0)
-            return s;
-        s.push_back(nums[0]);
-        
-        int sum;
-        // Add first element; 
-        
-        for(int i =0; i< nums.size()-1; i++)
+        vector<int>solution; 
+        int size = nums.size(); 
+        int sum; 
+        for(int i =0; i < size; i++)
         {
-            sum = 0;
-            for(int  j=0; j <=i+1; j++)
-            {
-                sum += nums[j];
-            }
-            s.push_back(sum);
+            sum += nums[i];
+            solution.push_back(sum);
         }
-        return s; 
+        return solution; 
     }
 };
