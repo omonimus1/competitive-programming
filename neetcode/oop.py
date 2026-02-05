fe01de68190d825a5a7c51978500dd25e0626607
Davide Pollicino <davidepollicino2015@gmail.com>
2026-02-05 23:06:48 +0100

    Create oop.py

---
+++ neetcode/oop.py
@@ -0,0 +1,19 @@
+- [https://algomaster.io/learn/lld/polymorphism](https://algomaster.io/learn/lld/polymorphism)
+- [https://www.geeksforgeeks.org/python/python-oops-exercise-questions/](https://www.geeksforgeeks.org/python/python-oops-exercise-questions/)
+* **Ineritance**: Inheritance allows one class (called the subclass or child class) to inherit the properties and behaviors of another class (called the superclass or parent class).
+* **Polymorphism** allows the same method name or interface to exhibit different behaviors depending on the object that is invoking it.
+* ** Encapsulation** is one of the four foundational principles of object-oriented design. It is the practice of grouping data (variables) and behavior (methods) that operate on that data into a single unit (typically a class) and restricting direct access to the internal details of that class.
+* **interface**: They define what a component should do, not how it should do it.
+
+## Class relationship
+- **Association** represents a relationship between two classes where one object uses, communicates with, or references another.
+One object need to know about the existence of another object to perform its responsibilities”
+Association reflects a "has-a" or "uses-a" relationship.
+- **Composition** is a special type of association that signifies strong ownership between objects. The “whole” class is fully responsible for creating, managing, and destroying the “part” objects. In fact, the parts cannot exist without the whole.
+
+Key Characteristics of Composition:
+Represents a strong “has-a” relationship.
+The whole owns the part and controls its lifecycle.
+When the whole is destroyed, the parts are also destroyed.
+The parts are not shared with any other object.
+The part has no independent meaning or identity outside the whole.
